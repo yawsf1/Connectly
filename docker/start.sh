e7d5299d3548e8d175bb37088ddafdcc4087c823
yawsf1 <yousseflaayadiasape2@gmail.com>
2026-05-09 21:59:10 +0100

    use port 8068

--- docker/start.sh
+++ docker/start.sh
@@ -3,9 +3,5 @@ php artisan migrate --force
 php artisan config:clear
 php artisan cache:clear
 php artisan storage:link --force
-
-# Replace port in nginx config
-sed -i "s/8080/${PORT:-8080}/g" /etc/nginx/sites-available/default
-
 php-fpm -D
 nginx -g 'daemon off;'
